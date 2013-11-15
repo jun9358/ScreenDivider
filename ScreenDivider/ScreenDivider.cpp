42dc5c8b7698beae6b7757453b59e0a6440a55a6
Jun-ho, Song <jun9358@gmail.com>
2013-11-16 00:48:01 +0900

    Set test environment
    To test hooker dll's function, enable InstallHook() function and show
    default dialog.

--- ScreenDivider/ScreenDivider.cpp
+++ ScreenDivider/ScreenDivider.cpp
@@ -72,7 +72,7 @@ CScreenDividerApp::CScreenDividerApp()
 
 	// TODO: add construction code here,
 	// Place all significant initialization in InitInstance
-	//InstallHook();
+	InstallHook();
 }
 
 
@@ -111,68 +111,7 @@ BOOL CScreenDividerApp::InitInstance()
 	// such as the name of your company or organization
 	SetRegistryKey(_T("Local AppWizard-Generated Applications"));
 
-	/*
-	CAeroDlg dlg;
-	m_pMainWnd = &dlg;
-	INT_PTR nResponse = dlg.DoModal();
-	if (nResponse == IDOK)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with OK
-	}
-	else if (nResponse == IDCANCEL)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with Cancel
-	}
-
-	// Delete the shell manager created above.
-	if (pShellManager != NULL)
-	{
-		delete pShellManager;
-	}
-	*/
-	/*
-	CEditorDlg dlg;
-	m_pMainWnd = &dlg;
-	INT_PTR nResponse = dlg.DoModal();
-	if (nResponse == IDOK)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with OK
-	}
-	else if (nResponse == IDCANCEL)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with Cancel
-	}
-	*/
-	/*
-	CFinderDlg dlg;
-	m_pMainWnd = &dlg;
-	INT_PTR nResponse = dlg.DoModal();
-	if (nResponse == IDOK)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with OK
-	}
-	else if (nResponse == IDCANCEL)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with Cancel
-	}
-
-	// Delete the shell manager created above.
-	if (pShellManager != NULL)
-	{
-		delete pShellManager;
-	}
-	*/
-	CSettingStyleDlg dlg2;
-	dlg2.Create(IDD_SETTING_STYLE_DIALOG);
-	dlg2.ShowWindow(TRUE);
-
-	CSettingGeneralDlg dlg;
+	CScreenDividerDlg dlg;
 	m_pMainWnd = &dlg;
 	INT_PTR nResponse = dlg.DoModal();
 	if (nResponse == IDOK)
